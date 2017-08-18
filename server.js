bac89c420bd2e37d8570eb15377630148413ce24
Jillian Pichocki <jpichocki@gmail.com>
2017-08-17 20:49:33 -0500

    create mustache profile template

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mustacheExpress = require("mustache-express");
 
 const app = express();
 const data = require("./data")
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8003;
 
 
 app.engine("mustache", mustacheExpress());
@@ -16,6 +16,12 @@ app.set("view engine", "mustache");
 
 app.get("/", (req, res) => {
     res.render("index", { users: data.users });
+});
+
+app.get("/:id", (req, res) => {
+    let userId = req.params.id;
+    let user = data.users.find(user => user.id === parseInt(userId));
+    res.render("profile", user);
 });
 
 
