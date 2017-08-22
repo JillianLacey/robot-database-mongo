27d85c9c628b6d5d10e46d00a616d0a9997bf364
Jillian Pichocki <jpichocki@gmail.com>
2017-08-22 13:51:59 -0500

    robot directory db started

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const mustacheExpress = require("mustache-express");
 
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+const ObjectId = mongo.ObjectID;
+const dbUrl = "mongodb://localhost:27017/introDemo";
+
 
 const app = express();
 const data = require("./data")
@@ -24,7 +29,28 @@ app.get("/:id", (req, res) => {
     res.render("profile", user);
 });
 
+//this is where we're importing data from data.js?
+app.get("/insertMany", function(req, res) {
+    MongoClient.connect(dbUrl, function(err, db) {
+      if (err) {
+        res.status(500).send(err);
+      }
+  
+      let People = db.collection("people");
+  
+      People.insertMany(people, function(err, savedPeople) {
+        if (err) {
+          res.status(500).send(err);
+        }
+  
+        res.send(savedPeople);
+        db.close();
+      });
+    });
+  });
+
+
 
 app.listen(port, () => {
-    // console.log(`Server is running on port ${port}!`);
+    console.log(`Server is running on port ${port}!`);
 });
